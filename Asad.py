b3dd553c8022e277c280a3e52971e19bd88dce80
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-21 19:20:19 +0530

    Update Asad.py

--- Asad.py
+++ Asad.py
@@ -4,6 +4,18 @@ from pytgcalls import PyTgCalls
 from pytgcalls import idle as pyidle
 from config import bot, call_py
 
+import pyrogram
+
+# Create a Pyrogram client with a file-based session storage
+client = pyrogram.Client(
+    "my_session",
+    api_id=25742938,
+    api_hash="b35b715fe8dc0a58e8048988286fc5b6",
+    storage=pyrogram.session.PersistentSession("path/to/session/file")
+)
+
+# Start the client
+client.start()
 
 print("UserBot Started")
 call_py.start()
