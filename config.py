bcba154afa0aecc864aff905177715eb611cda75
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-07-21 19:06:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,10 +8,10 @@ if os.path.exists(".env"):
     load_dotenv(".env")
 
 # Necessary Vars
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION = os.getenv("SESSION")
-HNDLR = os.getenv("HNDLR", "/")
+API_ID = 25742938
+API_HASH = "b35b715fe8dc0a58e8048988286fc5b6"
+SESSION = "BQGIzloATDrvWzXv7RRD6Ej6fCpW_B3JJhNYB9WFpzkM7U-g1YkBxxLE5F5ygIWKhtI7t1fuWoY65IHmpmIalZ8eWRTJiTCaEjtnxfhDWVbpEVGY-kgMTIb0j-GMNiSyKchdmIK_2Omlo1aUUTNqMZ3ir5-IfYgEA1jVdC1e10dP9TrX-Mxw_WU8qHPM4Yu10WC9MOPXBB-8wg5I1RPXN13NiF0i9KzkvEMM-66113IWF4zqDcVZxEIpt-WOGo-AxVjovHbjd9FJV1-WWpi5Ph4QIECUookVG6ZSGyln8z1JEh18zDvWD_FJhhrDU-9vqwJe0Io2s20-k1bjAYDYHww2IEk27QAAAAF5XDOMAA"
+HNDLR = /
 GROUP_MODE = os.getenv("GROUP_MODE", "True")
 
 
